1eb51739f9b48cb6d0f33ded8da6c99c8a863ec6
Pratibha Aggarwal <hiiamchristophrr@gmail.com>
2026-07-08 14:21:12 +0530

    leeee...

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 console.log("message")
 console.warn("this is a warning")
-console.error("this is error");
+console.error("this is error")
+confirm("Are you 18+?")
+alert("Be careful hehehe")
+/** confirm */
